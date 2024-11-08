83ff1842da7af74f174f9aeaabfd2e92d4985f0c
Tie0099 <161200326+Tie0099@users.noreply.github.com>
2024-11-08 15:34:30 +0700

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+module.exports = {
+  images: {
+    domains: ['firebasestorage.googleapis.com'],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      const antStyles = /antd\/.*?\/style.*?/;
+      config.externals = [
+        ...config.externals,
+        (context, request, callback) => {
+          if (request.match(antStyles)) return callback();
+          callback();
+        },
+      ];
 
-module.exports = nextConfig
+      config.module.rules.unshift({
+        test: antStyles,
+        use: 'null-loader',
+      });
+    }
+    return config;
+  },
+};
